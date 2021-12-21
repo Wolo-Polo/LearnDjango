b89d7086ea845b28dd74a3ca67534daa49ac45a9
Wolo <wolonguyen@gmail.com>
2021-12-21 18:15:49 +0700

    django4.0 intro tutorial04_2 use generic view class of django

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -2,41 +2,37 @@ from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.db.models import F
+from django.views import generic
 from .models import Question, Answer
 
 
 # Create your views here.
-def index(request):
-    list_questions = Question.objects.order_by("-datetime_create")[:5]
-    # lấy danh sách question, order by datetime_create, lấy 5 phần tử đầu
+class IndexView(generic.ListView):
+    template_name = "myapp/index.html"
+    context_object_name = "questions"
 
-    context = {
-        "questions": list_questions
-    }
-    return render(request, "myapp/index.html", context)
-
-
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, "myapp/detail.html", {"question": question})
+    def get_queryset(self):
+        # lấy danh sách question, order by datetime_create, lấy 5 phần tử đầu
+        return Question.objects.order_by("-datetime_create")[:5]
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "myapp/result.html", {"question": question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "myapp/detail.html"
+
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = "myapp/result.html"
 
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
         select_answer = question.answer_set.get(pk=request.POST["vote"])
-        #select_answer.votes += 1 # nếu sử dụng cách này 2 client vote cùng lúc có thể dẫn tới lỗi
+        # select_answer.votes += 1 # nếu sử dụng cách này 2 client vote cùng lúc có thể dẫn tới lỗi
         select_answer.votes = F("votes") + 1
         select_answer.save()
     except (KeyError, Answer.DoesNotExist):
         return render(request, "myapp/detail.html", {"error": "You didn't select a vote"})
     return HttpResponseRedirect(reverse("myapp:results", None, (question.id,)))
-
